91d3b7f158ab4416636122a63bf25a218337f2c7
L-Meinecke <152605350+L-Meinecke@users.noreply.github.com>
2023-12-04 14:42:45 +0100

    Add files via upload

---
+++ klasse_italienisches_essen.py
@@ -0,0 +1,18 @@
+class Italienisches_Essen:
+    anzahl = 0
+    list_sauce = ["Tomatensauce", "Hollandaisesauce", "Carbonarasauce"]
+    default_sauce = list_sauce[0]
+    scharf = bool(0)
+    extrakaese = bool(0)
+
+
+def __int__(self, anzahl, list_sauce, default_sauce, scharf, extrakaese):
+    self.anzahl = anzahl
+    self.list_sauce = list_sauce
+    self.default_sauce = default_sauce
+    self.scharf = scharf
+    self.extrakaese = extrakaese
+
+def essen_kochen():
+    pass
+
