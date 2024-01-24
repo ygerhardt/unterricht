cc7fa6419c80a15daa417e8f8d227806738a3da2
n0vedad <lucas.daemmig@gmx.net>
2024-01-24 11:19:19 +0100

    Update 24.01. (bugfixing, refactoring and update docstrings)

--- klasse_pizza.py
+++ klasse_pizza.py
@@ -17,19 +17,38 @@ class Pizza(Italienisches_Essen):
 
     def waehle_option(self):
         """
-        Ermöglicht dem Benutzer die Auswahl der Größe der Pizza.
+        Ermöglicht dem Benutzer die Auswahl der Größe der Pizza. Diese Methode ruft 'erfasse_optionen' auf, um dem 
+        Benutzer eine Auswahl an verschiedenen Pizzagrößen anzubieten, die in der Datenbank unter 'dict_pizza_groesse' 
+        definiert sind.
+
+        Die Methode gibt das ausgewählte Element zurück, welches die gewählte Pizzagröße und den dazugehörigen Preis 
+        enthält. Dies ist eine spezifische Implementierung der abstrakten Methode 'waehle_option' für die Pizza-Klasse.
+
+        Returns:
+            dict: Ein Dictionary, das die vom Benutzer ausgewählte Pizzagröße und deren Preis enthält. 
+            Das Dictionary enthält genau einen Eintrag mit der Beschreibung der Größe als Schlüssel und dem Preis als Wert.
         """
-        return self.erfasse_optionen(self.datenbank['dict_pizza_groesse'])
+        return self.erfasse_optionen('dict_pizza_groesse')
 
     def essen_kochen(self, zutaten):
         """
-        Stellt die Pizza zusammen, indem die ausgewählten Zutaten verarbeitet werden.
-        Die erste Zutat wird als die gewählte Pizzagröße betrachtet. Die weiteren Zutaten
-        werden zur Liste der Zutaten hinzugefügt, und die Zubereitungsdetails werden entsprechend aktualisiert.
+        Stellt die Pizza zusammen, indem die ausgewählten Zutaten für das Gericht verarbeitet werden. 
+        Diese Methode setzt die ausgewählte Pizzagröße und die weiteren Zutaten zusammen, um die 
+        endgültigen Zubereitungsdetails für die Pizza zu definieren.
+
+        Die ausgewählten Zutaten werden in einem Dictionary übergeben, wobei die erste Zutat als 
+        Pizzagröße betrachtet wird. Die weiteren Einträge im Dictionary repräsentieren zusätzliche 
+        Zutaten für die Pizza. Die Methode aktualisiert die internen Attribute der Klasse zur 
+        Speicherung der gesamten Zutatenliste und der Zubereitungsdetails.
 
         Args:
-            zutaten (dict): Ein Dictionary ausgewählter Zutaten für die Pizza mit ihren jeweiligen Preisen.
-                            Die erste Zutat wird als die gewählte Pizza-Größe betrachtet.
+            zutaten (dict): Ein Dictionary von ausgewählten Zutaten für die Pizza. Jeder Eintrag im 
+                            Dictionary repräsentiert eine Zutat mit ihrem Namen als Schlüssel und ihrem 
+                            Preis als Wert. Der erste Eintrag wird als die gewählte Pizzagröße betrachtet.
+
+        Returns:
+            None: Diese Methode gibt keinen Wert zurück, sondern aktualisiert die internen Attribute 
+                der Klasse bezüglich der Zutaten und Zubereitungsdetails.
         """
         self.zutaten = zutaten
         pizza_groesse = next(iter(zutaten.keys()))
--- oberklasse_italienisches_essen.py
+++ oberklasse_italienisches_essen.py
@@ -19,81 +19,101 @@ class Italienisches_Essen(ABC):
     @abstractmethod
     def essen_kochen(self, zutaten):
         """
-        Abstrakte Methode zum Kochen des Essens.
-        Muss in jeder Unterklasse implementiert werden.
+        Abstrakte Methode zum Zusammenstellen und "Kochen" des Essens. Diese Methode soll in jeder Unterklasse 
+        implementiert werden und ist verantwortlich für die Verarbeitung der ausgewählten Zutaten und die 
+        Zusammenstellung des Endprodukts (z.B. der fertigen Pizza oder Pasta).
+
+        In dieser Methode sollten die spezifischen Schritte definiert werden, die notwendig sind, um das 
+        Gericht basierend auf den ausgewählten Zutaten vorzubereiten. Dazu gehört die Verarbeitung der Zutatenliste, 
+        die Berechnung des Gesamtpreises und das Festlegen der endgültigen Zubereitungsdetails für das Gericht.
 
         Args:
-            zutaten (dict): Ausgewählte Zutaten für das Gericht.
+            zutaten (dict): Ein Dictionary der ausgewählten Zutaten für das Gericht. Jeder Eintrag im Dictionary 
+            repräsentiert eine Zutat mit ihrem Namen als Schlüssel und ihrem Preis als Wert. Die Implementierung in 
+            den Unterklassen kann je nach Art des Gerichts variieren und sollte die spezifischen Anforderungen und 
+            des jeweiligen Gerichtstyps berücksichtigen.
+
+        Returns:
+            None: Diese Methode soll keine Werte zurückgeben, sondern dient dazu, die internen Attribute der Klasse 
+            (wie die Liste der Zutaten und die Zubereitungsdetails) zu aktualisieren.
         """
         pass
 
     @abstractmethod
     def waehle_option(self):
         """
-        Abstrakte Methode zur Auswahl einer Option für das Gericht um mehrere Optionen zu unterstützen 
-        die in der main nicht extra spezifiziert werden müssen. Muss in jeder Unterklasse implementiert werden.
+        Abstrakte Methode zur Auswahl einer Option für das Gericht. Diese Methode soll in jeder Unterklasse 
+        implementiert werden und dient dazu, spezifische Optionen für verschiedene Gerichtstypen (wie Pizza oder Pasta) 
+        zu definieren und zu verarbeiten. 
+
+        Die Implementierung in den Unterklassen sollte die Benutzerinteraktion steuern, um eine Auswahl aus den 
+        verfügbaren Optionen zu treffen, die für den jeweiligen Gerichtstyp spezifisch sind (z.B. Pizzagröße oder 
+        Pastasorte). Diese Methode wird in Verbindung mit 'erfasse_optionen' verwendet, um eine 
+        konsistente und benutzerfreundliche Auswahlmöglichkeit zu bieten.
+
+        Returns:
+            dict: Ein Dictionary, das die vom Benutzer ausgewählte Option und deren Preis enthält.
         """
         pass
 
-    def erfasse_optionen(self, verfuegbareOptionen):
+    def erfasse_optionen(self, optionenTyp, mehrfachauswahl=False):
         """
         Erlaubt dem Benutzer, Optionen aus einem Dictionary von Optionen zu wählen.
-        Bei 'dict_zutaten' ist Mehrfachauswahl möglich, bei allen anderen Optionen nur Einzelauswahl.
+        Bei 'dict_zutaten' ist Mehrfachauswahl möglich, bei allen anderen Optionen ist nur eine Einzelauswahl gestattet.
+        Bei Mehrfachauswahl sind doppelte Optionen nicht erlaubt. Bei Einzelauswahl muss der Benutzer genau eine Option wählen.
+        Bei 'dict_scharf' und 'dict_extrakaese' wird eine Ja/Nein-Abfrage gestellt
 
         Args:
-            verfuegbareOptionen (dict): Ein Dictionary von Optionen, aus denen der Benutzer wählen kann.
+            optionenTyp (str): Ein Dictionary von Optionen, aus denen der Benutzer wählen kann.
+            mehrfachauswahl (bool): Gibt an, ob Mehrfachauswahl erlaubt ist.
 
         Returns:
             dict: Ausgewählte Optionen und ihre Preise.
         """
-        mehrfachauswahl = verfuegbareOptionen is self.datenbank['dict_zutaten']
+        verfuegbareOptionen = self.datenbank[optionenTyp]
         ausgewaehlteOptionen = {}
 
-        for key, value in verfuegbareOptionen.items():
-            print(f"{key}: {value['Beschreibung']} (Preis: {value['Preis']:.2f}\u20AC)")
+        if optionenTyp in ['dict_scharf', 'dict_extrakaese']:
+            for key, value in verfuegbareOptionen.items():
+                while True:
+                    auswahl = input(f"Möchten Sie {value['Beschreibung']} (Preis: {value['Preis']:.2f}\u20AC) hinzufügen? (ja/nein): ").lower()
+                    if auswahl == 'ja':
+                        ausgewaehlteOptionen[value['Beschreibung']] = value['Preis']
+                        break
+                    elif auswahl == 'nein':
+                        break
+                    else:
+                        print("\nUngültige Eingabe. Bitte antworten Sie mit 'ja' oder 'nein'.\n")
+        else:
+            while True:
+                for key, value in verfuegbareOptionen.items():
+                    print(f"{key}: {value['Beschreibung']} (Preis: {value['Preis']:.2f}\u20AC)")
 
-        eingabe = input("\nBitte wählen Sie eine oder mehrere Optionen (z.B. 01,03,05): " if mehrfachauswahl else "\nBitte wählen Sie eine Option (Nummer): ").split(',')
+                eingabe = input("\nBitte wählen Sie eine oder mehrere Optionen (z.B. 01,03,05): " if mehrfachauswahl else "\nBitte wählen Sie eine Option (Nummer): ").split(',')
 
-        for nummer in eingabe:
-            nummer = nummer.strip()
-            if nummer in verfuegbareOptionen:
-                ausgewaehlteOptionen[verfuegbareOptionen[nummer]['Beschreibung']] = verfuegbareOptionen[nummer]['Preis']
-                if not mehrfachauswahl:
+                if not mehrfachauswahl and len(eingabe) > 1:
+                    print("\nUngültige Auswahl. Bitte wählen Sie nur eine Option.\n")
+                    continue
+
+                if mehrfachauswahl and len(eingabe) != len(set(eingabe)):
+                    print("\nDoppelte Optionen sind nicht erlaubt. Bitte versuchen Sie es erneut.\n")
+                    continue
+
+                ungültigeAuswahl = False
+                for nummer in eingabe:
+                    nummer = nummer.strip()
+                    if nummer in verfuegbareOptionen:
+                        ausgewaehlteOptionen[verfuegbareOptionen[nummer]['Beschreibung']] = verfuegbareOptionen[nummer]['Preis']
+                    else:
+                        print(f"\nUngültige Auswahl: {nummer}. Bitte versuchen Sie es erneut.\n")
+                        ungültigeAuswahl = True
+                        break
+
+                if not ungültigeAuswahl:
                     break
-            else:
-                print(f"\nUngültige Auswahl. Bitte versuchen Sie es erneut.\n")
-                return self.erfasse_optionen(verfuegbareOptionen)
-
-        if not ausgewaehlteOptionen:
-            print("\nKeine gültigen Optionen ausgewählt. Bitte versuchen Sie es erneut.")
-            return self.erfasse_optionen(verfuegbareOptionen)
 
         return ausgewaehlteOptionen
 
-    def erfasse_zusatzoptionen(self, zusatzOptionenTyp):
-        """
-        Erfasst zusätzliche Optionen wie 'scharf' und 'extra Käse'.
-
-        Args:
-            zusatzOptionenTyp (str): Ein Dictionary von zusätzlichen Optionen, aus denen der Benutzer wählen kann.
-
-        Returns:
-            dict: Ausgewählte zusätzliche Optionen und ihre Preise.
-        """
-        zusatzOptionen = {}
-        for _, value in self.datenbank[zusatzOptionenTyp].items():
-            while True:
-                auswahl = input(f"\nMöchten Sie {value['Beschreibung']} hinzufügen? (ja/nein): ")
-                if auswahl.lower() == 'ja':
-                    zusatzOptionen[value['Beschreibung']] = value['Preis']
-                    break
-                elif auswahl.lower() == 'nein':
-                    break
-                else:
-                    print("\nUngültige Auswahl. Bitte geben Sie 'ja' oder 'nein' ein.")
-        
-        return zusatzOptionen
-    
     def erfasse_bestellung(self, hauptkomponente):
         """
         Erfasst eine Bestellung, indem sie die Hauptkomponente (z.B. Größe der Pizza oder Sorte der Pasta),
@@ -108,11 +128,13 @@ class Italienisches_Essen(ABC):
         Returns:
             dict: Die gesamte zusammengestellte Bestellung, einschließlich der Hauptkomponente, Zutaten, Sauce und zusätzlichen Optionen.
         """
-        verfuegbareZutaten = self.erfasse_optionen(self.datenbank['dict_zutaten'])
-        verfuegbareSaucen = self.erfasse_optionen(self.datenbank['dict_sauce'])
-        zusatzScharf = self.erfasse_zusatzoptionen('dict_scharf')
-        zusatzKaese = self.erfasse_zusatzoptionen('dict_extrakaese')
-        bestellung = {**hauptkomponente, **verfuegbareZutaten, **verfuegbareSaucen, **zusatzScharf, **zusatzKaese}
+        optionenTypen = ['dict_zutaten', 'dict_sauce', 'dict_scharf', 'dict_extrakaese']
+        mehrfachauswahl = [True, False, False, False]
+        bestellung = {**hauptkomponente}
+
+        for optionenTyp, mehrfach in zip(optionenTypen, mehrfachauswahl):
+            bestellung.update(self.erfasse_optionen(optionenTyp, mehrfach))
+
         self.essen_kochen(bestellung)
         
         return bestellung
