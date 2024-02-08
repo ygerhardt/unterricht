49faa01868657f780badbdc11b1a40c9f1ccf524
RaspberryLily <lily.jehn1996@gmail.com>
2024-02-08 13:40:43 +0100

    Grid for left side of gui

--- classes/gui/gui_class.py
+++ classes/gui/gui_class.py
@@ -18,42 +18,51 @@ class App(customtkinter.CTk):
         super().__init__()
         
         self.geometry(f"{1200}x{800}")
+        self.grid_rowconfigure((0), weight=1)
+        self.grid_columnconfigure(2, weight=1)
         self.title("Restaurant FIAE")
         
         
         #Images
-        logo_image = CTkImage(light_image=Image.open("gui\pictures\Papa python logo.png"),
-                              dark_image=Image.open("gui\pictures\Papa python logo.png"),
+        logo_image = CTkImage(light_image=Image.open(r"classes\gui\pictures\Papa python logo.png"),
+                              dark_image=Image.open(r"classes\gui\pictures\Papa python logo.png"),
                               size=(180,180))
 
-        pizza_label_icon = CTkImage(Image.open("gui\\pictures\\icons\\pizza_icon.png"), size=(60,60))
-        pasta_label_icon = CTkImage(Image.open("gui\\pictures\\icons\\pasta_icon.png"), size=(60,60))
-        dessert_label_icon = CTkImage(Image.open("gui\\pictures\\icons\\dessert_icon.png"), size=(60,60))
-        other_label_icon = CTkImage(Image.open("gui\\pictures\\icons\\beverage_icon.png"), size=(60,60))
+        pizza_label_icon = CTkImage(Image.open(r"classes\gui\pictures\icons\pizza_icon.png"), size=(60,60))
+        pasta_label_icon = CTkImage(Image.open(r"classes\gui\pictures\icons\pasta_icon.png"), size=(60,60))
+        dessert_label_icon = CTkImage(Image.open(r"classes\gui\pictures\icons\dessert_icon.png"), size=(60,60))
+        drink_label_icon = CTkImage(Image.open(r"classes\gui\pictures\icons\beverage_icon.png"), size=(60,60))
         
 
 
         #Frames within the main_window that are on the left side in the app
-        self.frame = CTkFrame(master=self, width=200, height=200, border_color="grey", border_width=2, corner_radius=12)
-        self.frame.place(relx=0.01, rely=0.01)
+        self.frame = CTkFrame(master=self, width=200, height=792, border_color="grey", border_width=2, corner_radius=12)
+        self.frame.grid(column=0, row=0, padx=(0,4), pady=4, sticky='nsew')
+        self.frame.grid_rowconfigure((0, 1), weight=0)
+        #self.frame.place(relx=0.01, rely=0.01)
 
-        self.logo_frame = CTkFrame(master=self.frame, width=190, height=190)
-        self.logo_frame.place(relx=0.024, rely=0.024)
+        self.logo_frame = CTkFrame(master=self.frame, width=170, height=170, border_color="grey", border_width=2, corner_radius=12)
+        self.logo_frame.grid(row=0, pady=(4,4), padx=4,)
+        self.logo_frame.grid_rowconfigure(0)
+        self.logo_frame.grid_columnconfigure(0)
+        #self.logo_frame.place(relx=0.024, rely=0.024)
 
-        self.logo_frame_label = CTkLabel(master = self.logo_frame, width=190, height=190, image=logo_image, text=None)
-        self.logo_frame_label.pack()
+        self.logo_frame_label = CTkLabel(master = self.logo_frame, width=170, height=170, image=logo_image, text=None)
+        self.logo_frame_label.grid(row=0,column=0, padx=4, pady=4)
+        #self.logo_frame_label.pack()
 
-        self.link_frame = CTkFrame(master=self, width=200, height=570, border_color="grey", border_width=2, corner_radius=12)
-        self.link_frame.place(relx=0.01, rely=0.275)
+        self.link_frame = CTkFrame(master=self.frame, width=186, height=590, border_color="grey", border_width=2, corner_radius=12)
+        self.link_frame.grid(row=1, sticky="ns", pady=(0,8), padx=4 )
+        self.link_frame.grid_rowconfigure((0,1,2,3,4,5), weight=1)
+        #self.link_frame.place(relx=0.01, rely=0.275)
         
         self.switch_frame = CTkFrame(master=self.link_frame, width=180,height=60, border_color= "grey", border_width=2, corner_radius=12)
-        self.switch_frame.place(relx=0.048, rely=0.88)
+        self.switch_frame.grid(row=5, column=0, padx=0, pady=0, sticky="nsew")
+        #self.switch_frame.place(relx=0.048, rely=0.88)
         
         self.light_mode_switch_frame =CTkFrame(master=self.switch_frame, width=180,height=160, border_color= "grey", border_width=2, corner_radius=12)
-        self.light_mode_switch_frame.place(relx=0.030, rely=0.074)
+        #self.light_mode_switch_frame.place(relx=0.030, rely=0.074)
         
-        self.contact_frame = CTkFrame(master=self.link_frame, width=200,height=30)
-        self.contact_frame.place(relx=0.14, rely=0.18)
 
      #Frames that are in the middle of the app
         self.middle_frame = CTkFrame(master=self, width=600, height=782, border_color="grey", border_width=2, corner_radius=12) 
@@ -73,9 +82,9 @@ class App(customtkinter.CTk):
         self.choose_pasta_frame.place(relx=0.022, rely=0.01)
         self.pasta_icon_frame=CTkFrame(master=self.choose_pasta_frame, width=80, height=80)
         self.pasta_icon_frame.place(relx=0.028,rely=0.05)
-        self.pizza_label=CTkLabel(master=self.pasta_icon_frame, image=pasta_label_icon, width=132, height=90, text="", cursor ="hand2")
-        self.pizza_label.bind("<Button-1>", self.open_url, True)
-        self.pizza_label.pack()
+        self.pasta_label=CTkLabel(master=self.pasta_icon_frame, image=pasta_label_icon, width=132, height=90, text="", cursor ="hand2")
+        self.pasta_label.bind("<Button-1>", self.open_url, True)
+        self.pasta_label.pack()
         
         #dessert frame
         self.choose_dessert_frame = CTkFrame(master=self.middle_frame, width=140, height=100, border_color="grey", border_width=2, corner_radius=12) 
@@ -86,14 +95,14 @@ class App(customtkinter.CTk):
         self.dessert_label.bind("<Button-1>", self.open_url, True)
         self.dessert_label.pack()
 
-        #other frame
-        self.choose_etc_frame = CTkFrame(master=self.middle_frame, width=140, height=100, border_color="grey", border_width=2, corner_radius=12) 
-        self.choose_etc_frame.place(relx=0.746, rely=0.01)
-        self.etc_icon_frame=CTkFrame(master=self.choose_etc_frame, width=80, height=80)
-        self.etc_icon_frame.place(relx=0.028,rely=0.05)
-        self.etc_label=CTkLabel(master=self.etc_icon_frame, image=other_label_icon, width=132, height=90, text="", cursor ="hand2")
-        self.etc_label.bind("<Button-1>", self.open_url, True)
-        self.etc_label.pack()
+        #drink frame
+        self.choose_drink_frame = CTkFrame(master=self.middle_frame, width=140, height=100, border_color="grey", border_width=2, corner_radius=12) 
+        self.choose_drink_frame.place(relx=0.746, rely=0.01)
+        self.drink_icon_frame=CTkFrame(master=self.choose_drink_frame, width=80, height=80)
+        self.drink_icon_frame.place(relx=0.028,rely=0.05)
+        self.drink_label=CTkLabel(master=self.drink_icon_frame, image=drink_label_icon, width=132, height=90, text="", cursor ="hand2")
+        self.drink_label.bind("<Button-1>", self.open_url, True)
+        self.drink_label.pack()
 
         self.scrollable_border_frame = CTkFrame(master=self.middle_frame, width=576, height=654, border_color="grey", border_width=2, corner_radius=12)
         self.scrollable_border_frame.place(relx=0.022, rely=0.150)
@@ -120,18 +129,6 @@ class App(customtkinter.CTk):
 
 
         #Button
-        self.contact_button = CTkButton(master=self.contact_frame, 
-                                        width=140, 
-                                        height=30, 
-                                        text="Kontakt", 
-                                        border_width=3,
-                                        border_color="#0a5c5c", 
-                                        corner_radius=20,  
-                                        fg_color="#7cbfb1", 
-                                        text_color="black",
-                                        hover=True, 
-                                        hover_color="#b3e6db")
-        self.contact_button.pack()
         
         self.light_mode_switch = CTkSwitch(master=self.light_mode_switch_frame, 
                                            width=170, height=50, 
